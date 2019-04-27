92a88a0f681d597c68d07e2a26df0a4e31918ab0
FangwenZhang <fz97@georgetown.edu>
2019-04-27 12:59:32 -0400

    data cleaning

--- First Test.R
+++ First Test.R
@@ -7,42 +7,80 @@ library(Quandl)
 #setting the key for the API
 Quandl.api_key("akHYXf6xiWgjt6itcz4X")
 
-newyorkdat <- as_tibble(Quandl("ZILLOW/C1_ZHVIAH")) %>% select(Date, "NewYork"=Value)
-bostondat <- as_tibble(Quandl("ZILLOW/C16_ZHVIAH")) %>% select(Date, "Boston"=Value)
+planodat <- as_tibble(Quandl("ZILLOW/C72_ZHVIAH")) %>% select(Date, "Plano"=Value)
+bostondat <- as_tibble(Quandl("ZILLOW/C22_ZHVIAH")) %>% select(Date, "Boston"=Value)
 chicagodat <- as_tibble(Quandl("ZILLOW/C3_ZHVIAH")) %>% select(Date, "Chicago"=Value)
-losangelesdat <- as_tibble(Quandl("ZILLOW/C2_ZHVIAH")) %>% select(Date, "LosAngeles"=Value)
-phillydat <- as_tibble(Quandl("ZILLOW/C4_ZHVIAH")) %>% select(Date, "Philadelphia"=Value)
-dcdat <- as_tibble(Quandl("ZILLOW/C18_ZHVIAH")) %>% select(Date, "DC" =Value)
+esterodat <- as_tibble(Quandl("ZILLOW/C2246_ZHVIAH")) %>% select(Date, "Estero"=Value)
+usdat <- as_tibble(Quandl("ZILLOW/M1_ZHVIAH")) %>% select(Date, "US"=Value)
+arlingtondat <- as_tibble(Quandl("ZILLOW/CO3401_ZHVIAH")) %>% select(Date, "Arlington" =Value)
 
-dat1 <- full_join(newyorkdat, bostondat, by=c("Date"))
-dat2 <- full_join(chicagodat, losangelesdat, by=c("Date"))
-dat3 <- full_join(phillydat, dcdat, by=c("Date"))
+
+dat1 <- full_join(planodat, bostondat, by=c("Date"))
+dat2 <- full_join(chicagodat, esterodat, by=c("Date"))
+dat3 <- full_join(usdat, arlingtondat, by=c("Date"))
 dat4<- full_join(dat1, dat2, by=c("Date"))
 dat <- full_join(dat4, dat3, by=c("Date")) %>% arrange(Date)
 dat
 
-dat[, -1] <- dat[, -1]/1000 #scaling everything (except dates) down by 1000
+dat <- cbind(dat[,1], (dat[,-1]/1000))
 
-
-#Using a loop to build different tibbles for each indexed year
+#Using a loop to build separate tibbles for each indexed year
 
 for(i in 1996:2018){
   x <- ifelse(i==1996, 1, ((i-1996)*12)-2)
   
   dat %>%
-    mutate(NewYork = NewYork/NewYork[x],
+    mutate(Plano = Plano/Plano[x],
            Boston = Boston/Boston[x],
            Chicago = Chicago/Chicago[x],
-           LosAngeles = LosAngeles/LosAngeles[x],
-           Philadelphia = Philadelphia/Philadelphia[x],
-           DC = DC/DC[x]) %>%
-    gather(key="City", value="Value", Boston, Chicago, DC, LosAngeles, NewYork, Philadelphia) %>%
+           Estero = Estero/Estero[x],
+           US = US/US[x],
+           Arlington = Arlington/Arlington[x]) %>%
+    gather(key="City", value="Value", Boston, Chicago, Arlington, Estero, Plano, US) %>%
     arrange(., Date) %>%
     assign(paste0("dat", i), ., envir = .GlobalEnv)
 }
 
-#also using a gather function for the non-indexed data
+#also using the gather function for the non-indexed data
 dattrue <- dat %>%
-  gather(key="City", value="Value", Boston, Chicago, DC, LosAngeles, NewYork, Philadelphia) %>%
+  gather(key="City", value="Value", Boston, Chicago, Arlington, Estero, Plano, US) %>%
   arrange(Date)
-dattrue
+
+#Creating the U.S.-adjusted data
+datadjust <- dat %>% mutate(Plano = Plano/US,
+                            Boston = Boston/US,
+                            Chicago = Chicago/US,
+                            Estero = Estero/US,
+                            Arlington = Arlington/US,
+                            US=US/US) %>%
+  select(Date, Arlington, Boston, Chicago, Estero, Plano, US) 
+
+datadjusttrue <-datadjust %>% gather(key="City", value="Value", Boston, Chicago, Arlington, Estero, Plano, US) %>%
+  arrange(., Date)
+
+#Creating the tibbles for each year for hte U.S.-adjusted data
+for(i in 1996:2018){
+  x <- ifelse(i==1996, 1, ((i-1996)*12)-2)
+  
+  datadjust %>%
+    mutate(Plano = Plano/Plano[x],
+           Boston = Boston/Boston[x],
+           Chicago = Chicago/Chicago[x],
+           Estero = Estero/Estero[x],
+           Arlington = Arlington/Arlington[x],
+           US=US/US[x]) %>%
+    gather(key="City", value="Value", Boston, Chicago, Arlington, Estero, Plano, US) %>%
+    arrange(., Date) %>%
+    assign(paste0("datadjust", i), ., envir = .GlobalEnv)
+}
+
+#creating a vector to manually assign colors later in the visualization 
+cols <- c("Boston" = "orangered2", 
+          "Chicago"= "gold2", 
+          "Plano"= "dodgerblue3", 
+          "Arlington"="magenta3", 
+          "Estero" = "steelblue1",
+          "US"="black")
+names <- c("Boston"="Boston, MA", "Chicago"= "Chicago, IL", 
+           "Plano"= "Plano, TX" , "Arlington" = "Arlington, VA", 
+           "Estero"= "Estero, FL", "US"= "U.S. Average")
